af8999ee02ba5a60a08ca8cbf7ddb7848856adb4
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-08 11:02:44 -0800

    add front end code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,41 @@ function Pizza(size, meats, cheeses, veggies, delivery) {
   this.price = 0
 }
 
-Pizza.prototype.orderUp(size, meats, cheeses, veggies, delivery) {
-  if (size === small){
+Pizza.prototype.orderUp = function(size, meats, cheeses, veggies, delivery) {
+  if (size === "small"){
     this.price = 7
-  } else if (size === regular) {
+  } else if (size === "regular") {
     this.price = 12
-  } else if (size === jumbo) {
+  } else if (size === "jumbo") {
     this.price = 20
   } else {
     alert("Please Pick a Size.")
   }
-  veggies.forEach(veggie) {
+  veggies.forEach(function(veggie) {
     this.price += .50
-  }
-  meats.forEach(meat) {
+  });
+  meats.forEach(function(meat) {
     this.price += 1.50
-  }
-  cheeses.forEach(cheese) {
+  });
+  cheeses.forEach(function(cheese) {
     this.price += 1
-  }
-  if (delivery === true) {
+  });
+  if (delivery === "delivery") {
     this.price += 5
   }
 }
 
-(document).ready(function() {
-  ("form#order").submit(function(event) {
+var newPizza = new Pizza ()
+
+$(document).ready(function() {
+  $("form#order").submit(function(event) {
     event.preventDefault();
-    
+    var inputSize = $("select#size").val();
+    var inputMeats = $("input:checkbox[name=meatType]:checked").val();
+    var inputCheeses = $("input:checkbox[name=cheeseType]:checked").val();
+    var inputVeggies = $("input:checkbox[name=veggieType]:checked").val();
+    var inputDelivery = $("select#orderType").val();
+
+    newPizza.orderUp(inputSize, inputMeats, inputCheeses, inputVeggies, inputDelivery);
   })
 })
