d05a69bf18fcf1e62a61918d21e2b809cd430863
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-08 12:06:53 -0800

    work on fixing forEach functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,42 @@
-function Pizza(size, meats, cheeses, veggies, delivery) {
+function Pizza(size, delivery) {
   this.size = size,
-  this.meats = meats,
-  this.cheeses = cheeses,
-  this.veggies = veggies,
+  this.meats = [],
+  this.cheeses = [],
+  this.veggies = [],
   this.delivery = delivery,
   this.price = 0
 }
 
-Pizza.prototype.orderUp = function(size, meats, cheeses, veggies, delivery) {
+Pizza.prototype.orderUp = function(size, delivery) {
+  console.log(this.price);
+  var veggies = this.veggies;
+  var meats = this.meats;
+  var cheeses = this.cheeses;
+  veggies.forEach(function(veggie) {
+    this.price += 1;
+    console.log(this.price);
+  });
+  meats.forEach(function(meat) {
+    this.price += 2;
+  });
+  cheeses.forEach(function(cheese) {
+    this.price += 1;
+  });
   if (size === "small"){
-    this.price = 7
+    this.price += 7
   } else if (size === "regular") {
-    this.price = 12
+    this.price += 12
   } else if (size === "jumbo") {
-    this.price = 20
+    this.price += 20
   } else {
     alert("Please Pick a Size.")
   }
-  veggies.forEach(function(veggie) {
-    this.price += .50
-  });
-  meats.forEach(function(meat) {
-    this.price += 1.50
-  });
-  cheeses.forEach(function(cheese) {
-    this.price += 1
-  });
   if (delivery === "delivery") {
     this.price += 5
+  } else {
+    return this.price;
+    console.log(this.price);
+
   }
 }
 
@@ -37,9 +46,18 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
     var inputSize = $("select#size").val();
-    var inputMeats = $("input:checkbox[name=meatType]:checked").val();
-    var inputCheeses = $("input:checkbox[name=cheeseType]:checked").val();
-    var inputVeggies = $("input:checkbox[name=veggieType]:checked").val();
+    var inputMeats = $("input:checkbox[name=meatType]:checked").each(function() {
+      var inputMeat = $(this).val();
+      newPizza.meats.push(inputMeat);
+    });
+    var inputCheeses = $("input:checkbox[name=cheeseType]:checked").each(function() {
+      var inputCheese = $(this).val();
+      newPizza.cheeses.push(inputCheese);
+    });
+    var inputVeggies = $("input:checkbox[name=veggieType]:checked").each(function() {
+      var inputVeggie = $(this).val();
+      newPizza.veggies.push(inputVeggie);
+    });
     var inputDelivery = $("select#orderType").val();
 
     newPizza.orderUp(inputSize, inputMeats, inputCheeses, inputVeggies, inputDelivery);
